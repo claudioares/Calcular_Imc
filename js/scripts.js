3b9e7972af6cc1dbe02e71a80d859c93f05cd324
claudioares <claudiojasoares.dev@gmail.com>
2022-12-07 22:59:49 -0300

    finalizando versão

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 const button = document.querySelector("#button");
+const buttonClear = document.querySelector(".clear");
+const height = document.querySelector("#altura");
+
+
+
+altura.addEventListener('keypress', ()=>{
+
+    let inputLength = height.value.length
+
+    if(inputLength === 1) height.value += '.'
+
+})
+
 
 
 
@@ -8,17 +21,34 @@ button.addEventListener("click", function(event){
     
     let peso = document.querySelector("#peso").value;
     let altura = document.querySelector("#altura").value;
+    let span_error = document.querySelector("#span_error");
     let imc;
-    
+
     
     if(!peso || !altura){
-        console.log('Preencha todos os dados!')
+
+        span_error.classList.remove('inative')
+        span_error.classList.add('active')
+
+        setTimeout(()=>{
+            span_error.classList.remove('active')
+            span_error.classList.add('inative')
+        }, 2000)
+        
+        return;
     }
     
     imc = peso / (altura * altura);
     imc = imc.toFixed(2)
     
-    let imc_result = document.querySelector(".imc_result_p").textContent = imc;
-    console.log(imc)
+    document.querySelector(".imc_result_p").textContent = imc;
+   
+    
+    buttonClear.addEventListener("click"), (e) => {
+        event.preventDefault(e)
+        document.querySelector("#peso").value = '';
+        document.querySelector("#altura").value = '';
+        document.querySelector(".imc_result_p").textContent = '';
+    }
 
 })
